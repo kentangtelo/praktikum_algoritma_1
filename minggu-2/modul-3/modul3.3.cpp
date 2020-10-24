ed96e28b41ec2d9a88da9b20a9a29c032f19a28c
kentangtelo <namasayaiman02@gmail.com>
2020-10-24 15:35:56 +0700

    mengumpulkan tugas minggu-2

---
+++ minggu-2/modul-3/modul3.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	int x = 12; 
+    int y = 8; 
+    int hasil = x + y;
+    cout << "X : " << x << "\n";    
+    cout << "Y   : " << y << "\n";
+    cout << "Hasil    : " << hasil << "\n";
+    return 0;
+}
