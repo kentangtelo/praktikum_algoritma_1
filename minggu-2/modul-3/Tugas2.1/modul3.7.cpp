f1fe9dfb2563ed52c67167189851411f102331df
kentangtelo <namasayaiman02@gmail.com>
2020-10-26 08:54:05 +0700

    mengumpulkan tugas minggu-2

---
+++ minggu-2/modul-3/Tugas2.1/modul3.7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a = 0, b = 0, x = 0, y = 35;
+cout << "Nilai awal adalah";
+cout << "\na : " << a; 
+cout << "\nb : " << b; 
+cout << "\nx : " << x; 
+cout << "\ny : " << y; cout << endl;
+
+cout << "\nNilai setelah masing-masing variabel di ubah menjadi";
+cout << "\na : "<< a; 
+cout << "\nb : " << b; 
+cout << "\nx : " << x; 
+cout << "\ny : " << y; 
+return 0;
+}
---
+++ minggu-2/modul-3/Tugas2.2.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iostream> 
+using namespace std; 
+
+int main()
+{
+cout << "Nilai dari 5 + 3 = " << 9+4;       
+cout << "\nNilai dari 5 - 3 = " << 9-4;    
+cout << "\nNilai dari 5 * 3 = " << 9*4;     
+cout << "\nNilai dari 5 div 3 = " << 9/4;
+cout << "\nNilai dari 5 / 3 = " << 9.0/4.0; 
+cout << "\nNilai dari 5 mod 3 = " << 9%4; 
+ return 0;
+}
